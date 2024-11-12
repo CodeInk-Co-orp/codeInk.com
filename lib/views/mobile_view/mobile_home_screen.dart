ddc7678a16cef3b89a8815317c2251c93139a91f
Dev-Kibiwot <denniskibiwott2001@gmail.com>
2024-11-12 17:40:34 +0300

    v0.0.3

--- lib/views/mobile_view/mobile_home_screen.dart
+++ lib/views/mobile_view/mobile_home_screen.dart
@@ -1,4 +1,14 @@
 import 'package:codink_coop/controllers/home_screen_controller.dart';
+import 'package:codink_coop/services/get_theme.dart';
+import 'package:codink_coop/utils/themes.dart';
+import 'package:codink_coop/views/desktop_view/sections/about_us_section.dart';
+import 'package:codink_coop/views/desktop_view/sections/introduction_section.dart';
+import 'package:codink_coop/views/desktop_view/sections/navigation_section.dart';
+import 'package:codink_coop/views/desktop_view/sections/our_projects_section.dart';
+import 'package:codink_coop/views/desktop_view/sections/service_section.dart';
+import 'package:codink_coop/views/desktop_view/sections/testimonial_section.dart';
+import 'package:codink_coop/views/mobile_view/sections/about_mobile_section.dart';
+import 'package:codink_coop/widgets/custom_spacing.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,9 +19,32 @@ class MobileHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: SafeArea(
-        child: Container(),
+      backgroundColor: isLightTheme(context) ? KColors.white : KColors.black,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          NavigationSection(homeScreenController: homeScreenController),          
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                // shrinkWrap: true,
+                // physics: ClampingScrollPhysics(),
+                children: [
+                  // Introduction Section
+                  IntroductionSection(introductionGlobalKey: homeScreenController.introductionGlobalKey),
+                  // About Us Section
+                  AboutMobileSection(aboutMeGlobalKey: homeScreenController.aboutMeGlobalKey,),
+                  // Service  Section
+                  ServiceSection(serviceGlobalKey: homeScreenController.serviceGlobalKey),
+                  // Portfolio Section
+                  OurProjectsSection(portfolioGlobalKey: homeScreenController.portfolioGlobalKey),
+                  TestimonialSection(testimonialsGlobalKey: homeScreenController.testimonialsGlobalKey),
+                  CustomSpacing(height: .05),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/views/mobile_view/sections/about_mobile_section.dart
@@ -0,0 +1,139 @@
+import 'package:codink_coop/services/get_theme.dart';
+import 'package:codink_coop/utils/themes.dart';
+import 'package:codink_coop/views/layout_screen.dart';
+import 'package:codink_coop/widgets/custom_spacing.dart';
+import 'package:codink_coop/widgets/space.dart';
+import 'package:codink_coop/widgets/text.dart';
+import 'package:flutter/material.dart';
+
+class AboutMobileSection extends StatelessWidget {
+  const AboutMobileSection({super.key, required this.aboutMeGlobalKey});
+  final GlobalKey aboutMeGlobalKey;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      key: aboutMeGlobalKey,
+      padding: EdgeInsets.symmetric(
+        horizontal: horizontalSpace(context, .025),
+        vertical: verticalSpace(context, .02)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomSpacing(
+            height: 0.02,
+          ),
+          CustomText(
+            text: "About US", 
+            fontSize: 30, 
+            textColor: isLightTheme(context) ? KColors.black : KColors.white,
+            fontWeight: FontWeight.w500,
+          ),
+          CustomSpacing(
+            height: 0.02,
+          ),
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: isLightTheme(context) ? KColors.white : KColors.black,
+            ),
+            child: !LayoutScreen.isMediumScreen(context) ? Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: verticalSpace(context, .65) < 350 ? 350 : verticalSpace(context, .65),
+                  width: horizontalSpace(context, 0.4),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: AssetImage(
+                        "assets/images/office.jpg",
+                      ),
+                      fit: BoxFit.cover
+                    )
+                  ),
+                ),
+                SizedBox(width: horizontalSpace(context, 0.025)),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CustomText(
+                          text: '''At CodeInk Solutions, we are passionate about transforming ideas into digital experiences that engage and inspire.Our team is composed of skilled developers, designers, and strategists who specialize in crafting custom software solutions,from mobile and web applications to graphic design and branding. With a focus on both functionality and aesthetics,we bring expertise across multiple domains, including Android and iOS app development, web applications,API and payment integrations, and graphic design.''', 
+                          fontSize: 18,
+                          textAlign: TextAlign.start,
+                          textColor: isLightTheme(context) ? KColors.black : KColors.lightGrey,
+                        ),
+                        CustomSpacing(height: .05),
+                        CustomText(
+                          text: "Our Mission", 
+                          fontSize: 30, 
+                          textColor: isLightTheme(context) ? KColors.black : KColors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        CustomText(
+                          text: "To empower businesses and individuals through technology. By leveraging the latest tools and industry best practices, we deliver solutions that are not only innovative but also user-centered and reliable. Whether we're building a mobile app that delivers a seamless cross-platform experience or designing a logo that resonates with your brand, we work closely with our clients to ensure that every project aligns with their goals and vision.", 
+                          fontSize: 18,
+                          textAlign: TextAlign.start,
+                          textColor: isLightTheme(context) ? KColors.black : KColors.lightGrey,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(width: horizontalSpace(context, 0.05)),
+              ],
+            ) : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Center(
+                    child: CustomText(
+                      text: '''At CodeInk Solutions, we are passionate about transforming ideas into digital experiences that engage and inspire.Our team is composed of skilled developers, designers, and strategists who specialize in crafting custom software solutions,from mobile and web applications to graphic design and branding. With a focus on both functionality and aesthetics,we bring expertise across multiple domains, including Android and iOS app development, web applications,API and payment integrations, and graphic design.''', 
+                      fontSize: 18,
+                      textAlign: TextAlign.start,
+                      textColor: isLightTheme(context) ? KColors.black : KColors.lightGrey,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: verticalSpace(context, .65) < 350 ? 350 : verticalSpace(context, .4),
+                  width: horizontalSpace(context, 1), 
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: AssetImage(
+                        "assets/images/office.jpg",
+                      ),
+                      fit: BoxFit.cover
+                    )
+                  ),
+                ),
+                SizedBox(width: horizontalSpace(context, 0.025)),
+                CustomSpacing(height: .025),
+                CustomText(
+                  text: "Our Mission", 
+                  fontSize: 30, 
+                  textColor: isLightTheme(context) ? KColors.black : KColors.white,
+                  fontWeight: FontWeight.w500,
+                ),
+                CustomText(
+                  text: "To empower businesses and individuals through technology. By leveraging the latest tools and industry best practices, we deliver solutions that are not only innovative but also user-centered and reliable. Whether we're building a mobile app that delivers a seamless cross-platform experience or designing a logo that resonates with your brand, we work closely with our clients to ensure that every project aligns with their goals and vision.", 
+                  fontSize: 18,
+                  textAlign: TextAlign.start,
+                  textColor: isLightTheme(context) ? KColors.black : KColors.lightGrey,
+                ),
+              ],
+            ),
+          ),          
+        ],
+      ),
+    );
+  }
+}
